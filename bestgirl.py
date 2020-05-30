c455ddc28ad3012d3e73c7338f2802fb8bfc1013
Prox <39493761+The-Proxster@users.noreply.github.com>
2020-05-30 13:31:08 -0400

    Create bestgirl.py
    Here you people go.

---
+++ bestgirl.py
@@ -0,0 +1,36 @@
+# This shows who best girl is, very simple.
+bestgirlchoices = ['Akame']
+ihavespoken = """dmNNNNNmhs+/:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::///////
+mmNNNNmdyo/:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::////
+mmNNNNmds+/:::::::::::::::::::::::::::::::::::::::::::/+o+/:::::::::::::::::::::::::::::::::::::////
+mmNNNmmhs/:::::::::::::::::::::::::::::::::::::::/+syhyhdddyooo+/::::::::::::::::::::::::::::::::://
+mNNNNmdy+/::::----:::::::::::::::::::::::::::::::+dmmmhdmdmhhdmmdo::::::::::::::::::::::::::::::::::
+mNNNmmds+::------------:::::::::::::::::::::::+ooydddhhdddddddddhy+:::::::::::::::::::::::::::::::::
+mNNNmmho/:-----------------------------::::::+shmmdddddhhhhhddmmmdo:::::::::::::::::::::::::::::::::
+mmNNmmho:----------------------------------:syhsssyhhhdddhhhhhhhhdy:::::::::::::::::::::::::::::::::
+mmNNmmhs/::-----------------------------:::soyo/:::+oyhhddhhhhhyyhh:---------::::::::::::::-::::::::
+mmmmmdhhyo+::---------------------------/yodhyo/:/+oshdddddddhhyyhhho/:---------------------:::::::/
+mmmmdhhhhhys+:--------------------------oddddss+syhdmmmmdddmmmmdhddmmho------------------::::///////
+mddddddddddhyo/:------------------------odmmmhdooshhdddhdddddddhhddmmdy------------::::::://++++++++
+mmddddddmmdddhs/:-----------------------ohmmmddhssyoo+odmddmdhhhhmdmmmo---------:::////////+++oooooo
+mmdddddddmmmddhs+:--------------------:/ohdddddhoo+s//+ohhhdhhhhhdmmds:----::::///+++++++///+ossyyss
+Nmdddddddmmmmdddy+:-------------------/oyhohoyyy++oo++oydddddhyyhhdmmdyo/://++++++++++++///++osyyyyy
+Nmmmmmdmmmmmmmmmdyo/:-----:::::------:+yds+sohy//+ysoshdmmmmmdhhdhhmNNNmdhooooo+++++++++++++osyyyyyy
+NmmmmmmmmmmmmmmNmdhs+/::::::::::/+//:/sdmsooohho/sdddmddddddhhddmdhdmNNNNmmysoo++++++++++++oossyyyys
+NmmmmmmmmmmmNNmNNmdhs+/////////sdmds/ohdy++ooshddmNNmmmddddddddmNNmmmNNNNNmmdyo++++++++++ooossssysso
+NNmmmmmmmmmmNmmNNmmdhso+/////+hmNNms+oys+++++oyhhdddmmNNNmmmmmmmNNNNNmNNNNNmmms++/+++++++++oosoooo++
+NNmmmmmmmmmmNNNNNmmddyo++/+ooydmddms++o+++++osyddddhhdmmNNNNNNNNNNmmNNmNNNNNmmdy+//++++////++o++++++
+NNNmmmmmmmmmmNNNNmmmdhyo++oooyddmmNs+ooo+oooydmmmmmmmmmmmNNNNNNNNNmmNNmmNNNNmmmms//////////////++++o
+NNNNNmmmmmmmmNNNNNmmmdhso+++++ydmmms+oossyyhdmmmmNNNNmmmmmNNNNNNNmNNNNNNNNNNNmmNmhs+////////++++oooo
+NNNNNNmmmmmmmmNNNNNmmmhso+++++shmmmhhdddhdhdmmmmmmNNNNNNNmmmNNNNNNNNNNNNNNNNNmNNmmmmhhs+oooossssyyyy
+NNNNNNNNNmmmNNNNNNNmmmdyo+++++ommddmNNNdNNhmmmmmmmmmNNNNNNmmmmmdNNNNNNNNNmmNNNNNNNNNNNmhyyyyyhhyyyyy
+NNNNNNNNNNNNNNNNNNNmmmdhs+++++yNNhdmNNNhNNyhmdymhdhmmNmdmhymdhmydhmmhdmNNNNNNNNNNNNNNNNNNdhhyysoooos
+NNNNNNNNNNNNNNNNNNNmmmmhso++++dNNhdmNNNNNmmmmmmmmmmmNNmmmdNNmNNmmNmNNNNNNNNNNNNNNNNNNNNNNNdyso++++os
+NNNNNNNNNNNNNNNNNNNmmmmdyo+++omNNhmmNNNNNmmmmmmmmmmmmmmNNNNNNNNNNNNNNNNNNNNNmmNNNNNNNNNNNNh+++++ooss
+NNNNNNNNNNNNNNNNNNNNmmmdyo++osmNmhNNNNNNNNmmmhhddmmmmmNmNNNmmNNNNNNNNNNNNNNNhyNNNNNNNNNNNNmooosyysss"""
+
+print("The best girl is:")
+for x in bestgirlchoices:
+    print(x)
+print(ihavespoken)
+print("I have spoken.")
